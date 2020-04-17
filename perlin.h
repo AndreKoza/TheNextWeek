a5f57d406c3b4593cdb3b58c455ec7a260b90471
André <42980905+AndreKoza@users.noreply.github.com>
2020-04-17 10:00:50 +0200

    Added trilinear interpolation to perlin noise

--- perlin.h
+++ perlin.h
@@ -2,6 +2,24 @@
 
 #include "rtweekend.h"
 
+inline double trilinear_interp(double c[2][2][2], double u, double v, double w)
+{
+	auto accum = 0.0;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			for (int k = 0; k < 2; k++)
+			{
+				accum += (i * u + (1 - i) * (1 - u)) *
+					(j * v + (1 - j) * (1 - v)) *
+					(k * w + (1 - k) * (1 - w)) * c[i][j][k];
+			}
+		}
+	}
+	return accum;
+}
+
 class perlin
 {
 	public:
@@ -32,11 +50,21 @@ class perlin
 			auto v = p.y() - std::floor(p.y());
 			auto w = p.z() - std::floor(p.z());
 
-			auto i = static_cast<int>(4 * p.x()) & 255;
-			auto j = static_cast<int>(4 * p.y()) & 255;
-			auto k = static_cast<int>(4 * p.z()) & 255;
+			auto i = static_cast<int>(floor(p.x()));
+			auto j = static_cast<int>(floor(p.y()));
+			auto k = static_cast<int>(floor(p.z()));
+			double c[2][2][2];
 
-			return ranfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+			for (int di = 0; di < 2; di++)
+				for (int dj = 0; dj < 2; dj++)
+					for (int dk = 0; dk < 2; dk++)
+						c[di][dj][dk] = ranfloat[
+							perm_x[(i + di) & 255] ^
+								perm_y[(j + dj) & 255] ^
+								perm_z[(k + dk) & 255]
+						];
+
+			return trilinear_interp(c, u, v, w);
 		}
 
 
@@ -72,3 +100,5 @@ class perlin
 			}
 		}
 };
+
+
