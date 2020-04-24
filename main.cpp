4d7f281b63aa34f7acd8aa50f927cdb9dac989ab
André <42980905+AndreKoza@users.noreply.github.com>
2020-04-24 11:04:56 +0200

    Added fog / participating media and the final scene from the book

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include "moving_sphere.h"
 #include "rtw_stb_image.h"
 #include "box.h"
+#include "constant_medium.h"
 
 
 vec3 ray_color(const ray& r, const vec3& background, const hittable &world, int depth)
@@ -203,9 +204,177 @@ hittable_list cornell_box()
     box2 = make_shared<rotate_y>(box2, -18);
     box2 = make_shared<translate>(box2, vec3(130, 0, 65));
     objects.add(box2);
-
-    //objects.add(make_shared<sphere>(vec3(130, 50, 65), 50.0, make_shared<dielectric>(1.5)));
     
+    return objects;
+}
+
+hittable_list cornell_balls() 
+{
+    hittable_list objects;
+
+    auto pertext = make_shared<noise_texture>(4);
+    auto perlin = make_shared<lambertian>(pertext);
+
+    auto red = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.65, 0.05, 0.05)));
+    auto white = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.73, 0.73, 0.73)));
+    auto green = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.12, 0.45, 0.15)));
+    auto light = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(5, 5, 5)));
+
+    objects.add(make_shared<flip_face>(make_shared<yz_rect>(0, 555, 0, 555, 555, green)));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(113, 443, 127, 432, 554, light));
+    objects.add(make_shared<flip_face>(make_shared<xz_rect>(0, 555, 0, 555, 555, white)));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<flip_face>(make_shared<xy_rect>(0, 555, 0, 555, 555, white)));
+
+    auto boundary = make_shared<sphere>(vec3(160, 100, 145), 100, make_shared<dielectric>(1.5));
+    objects.add(boundary);
+    objects.add(make_shared<constant_medium>(
+        boundary, 0.01, make_shared<constant_texture>(vec3(0.12, 0.12, 0.5))
+        ));
+
+    auto boundary2 = make_shared<sphere>(vec3(380, 100, 50), 100, make_shared<dielectric>(1.5));
+    objects.add(boundary2);
+
+    shared_ptr<hittable> box1 = make_shared<box>(vec3(0, 0, 0), vec3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+    objects.add(box1);
+
+    return objects;
+}
+
+hittable_list cornell_smoke()
+{
+    hittable_list objects;
+
+    auto red = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.65, 0.05, 0.05)));
+    auto white = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.73, 0.73, 0.73)));
+    auto green = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.12, 0.45, 0.15)));
+    auto light = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(7, 7, 7)));
+
+    objects.add(make_shared<flip_face>(make_shared<yz_rect>(0, 555, 0, 555, 555, green))); // left
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red)); // right
+    objects.add(make_shared<xz_rect>(113, 443, 127, 432, 554, light));
+    objects.add(make_shared<flip_face>(make_shared<xz_rect>(0, 555, 0, 555, 555, white))); // top
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white)); // bottom
+    objects.add(make_shared<flip_face>(make_shared<xy_rect>(0, 555, 0, 555, 555, white))); // back
+
+    shared_ptr<hittable> box1 = make_shared<box>(vec3(0, 0, 0), vec3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+
+    shared_ptr<hittable> box2 = make_shared<box>(vec3(0, 0, 0), vec3(165, 165, 165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130, 0, 65));
+
+    objects.add(make_shared<constant_medium>(box1, 0.01, make_shared<constant_texture>(vec3(0, 0, 0))));
+    objects.add(make_shared<constant_medium>(box2, 0.01, make_shared<constant_texture>(vec3(1, 1, 1))));
+
+    return objects;
+}
+
+hittable_list cornell_final() 
+{
+    hittable_list objects;
+
+    auto pertext = make_shared<noise_texture>(0.1);
+
+    int nx, ny, nn;
+    unsigned char* tex_data = stbi_load("earthmap.jpg", &nx, &ny, &nn, 0);
+
+    auto mat = make_shared<lambertian>(make_shared<image_texture>(tex_data, nx, ny));
+
+    auto red = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.65, 0.05, 0.05)));
+    auto white = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.73, 0.73, 0.73)));
+    auto green = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.12, 0.45, 0.15)));
+    auto light = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(7, 7, 7)));
+
+    objects.add(make_shared<flip_face>(make_shared<yz_rect>(0, 555, 0, 555, 555, green)));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(123, 423, 147, 412, 554, light));
+    objects.add(make_shared<flip_face>(make_shared<xz_rect>(0, 555, 0, 555, 555, white)));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<flip_face>(make_shared<xy_rect>(0, 555, 0, 555, 555, white)));
+
+    shared_ptr<hittable> boundary2 =
+        make_shared<box>(vec3(0, 0, 0), vec3(165, 165, 165), make_shared<dielectric>(1.5));
+    boundary2 = make_shared<rotate_y>(boundary2, -18);
+    boundary2 = make_shared<translate>(boundary2, vec3(130, 0, 65));
+
+    auto tex = make_shared<constant_texture>(vec3(0.9, 0.9, 0.9));
+
+    objects.add(boundary2);
+    objects.add(make_shared<constant_medium>(boundary2, 0.2, tex));
+
+    return objects;
+}
+
+hittable_list final_scene()
+{
+    hittable_list boxes1;
+    auto ground = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.48, 0.83, 0.53)));
+
+    const int boxes_per_side = 20;
+    for (int i = 0; i < boxes_per_side; ++i)
+    {
+        for (int j = 0; j < boxes_per_side; ++j)
+        {
+            auto w = 100;
+            auto x0 = -1000.0 + i * w;
+            auto z0 = -1000.0 + j * w;
+            auto y0 = 0.0;
+            auto x1 = x0 + w;
+            auto y1 = random_double(1, 101);
+            auto z1 = z0 + w;
+
+            boxes1.add(make_shared<box>(vec3(x0, y0, z0), vec3(x1, y1, z1), ground));
+        }
+    }
+
+    hittable_list objects;
+
+    objects.add(make_shared<bvh_node>(boxes1, 0, 1));
+
+    auto light = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(7, 7, 7)));
+    objects.add(make_shared<xz_rect>(123, 423, 147, 412, 554, light));
+
+    auto center1 = vec3(400, 400, 200);
+    auto center2 = center1 + vec3(30, 0, 0);
+    auto moving_sphere_material =
+        make_shared<lambertian>(make_shared<constant_texture>(vec3(0.7, 0.3, 0.1)));
+    objects.add(make_shared<moving_sphere>(center1, center2, 0, 1, 50, moving_sphere_material));
+
+    objects.add(make_shared<sphere>(vec3(260, 150, 45), 50, make_shared<dielectric>(1.5)));
+    objects.add(make_shared<sphere>(vec3(0, 150, 145), 50, make_shared<metal>(vec3(0.8, 0.8, 0.9), 10.0)));
+
+    auto boundary = make_shared<sphere>(vec3(360, 150, 145), 70, make_shared<dielectric>(1.5));
+    objects.add(boundary);
+    objects.add(make_shared<constant_medium>(boundary, 0.2, make_shared<constant_texture>(vec3(0.2, 0.4, 0.9))));
+
+    boundary = make_shared<sphere>(vec3(0, 0, 0), 5000, make_shared<dielectric>(1.5));
+    objects.add(make_shared<constant_medium>(boundary, 0.0001, make_shared<constant_texture>(vec3(1, 1, 1))));
+
+    int nx;
+    int ny;
+    int nn;
+    auto tex_data = stbi_load("earthmap.jpg", &nx, &ny, &nn, 0);
+    auto emat = make_shared<lambertian>(make_shared<image_texture>(tex_data, nx, ny));
+    objects.add(make_shared<sphere>(vec3(400, 200, 400), 100, emat));
+
+    auto pertext = make_shared<noise_texture>(4);
+    objects.add(make_shared<sphere>(vec3(220, 280, 300), 80, make_shared<lambertian>(pertext)));
+
+    hittable_list boxes2;
+    auto white = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.73, 0.73, 0.73)));
+    int ns = 1000;
+    for (int j = 0; j < ns; ++j)
+    {
+        boxes2.add(make_shared<sphere>(vec3::random(0, 165), 10, white));
+    }
+
+    objects.add(make_shared<translate>(make_shared<rotate_y>(make_shared<bvh_node>(boxes2, 0.0, 1.0), 15), vec3(-100, 270, 395)));
+
     return objects;
 }
 
@@ -217,7 +386,7 @@ int main()
 
     const int image_width = 600;
     const int image_height = 600;
-    const int samples_per_pixel = 1000;
+    const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     const auto aspect_ratio = double(image_width) / double(image_height);  
@@ -243,7 +412,7 @@ int main()
     vec3 background(Color::black);
     auto world = random_scene();
 
-    switch (6)
+    switch (10)
     {
     case 1:
         world = random_scene();
@@ -284,10 +453,37 @@ int main()
         vfov = 20.0;
         break;
 
-    default:
     case 6:
         world = cornell_box();
         lookfrom = vec3(278, 278, -800);
+        lookat = vec3(278, 278, 0);
+        vfov = 40.0;
+        break;
+
+    case 7:
+        world = cornell_balls();
+        lookfrom = vec3(278, 278, -800);
+        lookat = vec3(278, 278, 0);
+        vfov = 40.0;
+        break;
+
+    case 8:
+        world = cornell_smoke();
+        lookfrom = vec3(278, 278, -800);
+        lookat = vec3(278, 278, 0);
+        vfov = 40.0;
+        break;
+
+    case 9:
+        world = cornell_final();
+        lookfrom = vec3(278, 278, -800);
+        lookat = vec3(278, 278, 0);
+        vfov = 40.0;
+        break;
+
+    case 10:
+        world = final_scene();
+        lookfrom = vec3(478, 278, -600);
         lookat = vec3(278, 278, 0);
         vfov = 40.0;
         break;
